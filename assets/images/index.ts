3cf28523b5863972a806b9615a53a715150dc5d9
aditya <adityabalsane10@gmail.com>
2024-08-26 01:28:56 +0530

    asset changes

---
+++ assets/images/index.ts
@@ -0,0 +1,13 @@
+export const Ava = {
+  annoyed: require('./annoyed.png'),
+  blushing: require('./blushing.png'),
+  confused: require('./confused.png'),
+  default: require('./default.png'),
+  embarrassed: require('./embarassed.png'),
+  glad: require('./glad.png'),
+  defaultAlt: require('./happy.png'),
+  sad: require('./sad.png'),
+  shy: require('./shy.png'),
+  thinking: require('./thinking.png'),
+  shocked: require('./shocked.png')
+}
