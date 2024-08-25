66ed2de4c94a0e4c9bfcf47ab35e467dd3356e94
aditya <adityabalsane10@gmail.com>
2024-08-26 01:27:30 +0530

    added expo-speech

--- hooks/useTTS.tsx
+++ hooks/useTTS.tsx
@@ -1,14 +1,35 @@
-import {StyleSheet, Text, View} from 'react-native'
-import React from 'react'
+import {useEffect, useState, useCallback, SetStateAction} from 'react'
+import * as Speech from 'expo-speech'
 
-const useTTS = () => {
-  return (
-    <View>
-      <Text>useTTS</Text>
-    </View>
-  )
+const useTTS = (
+  text: string,
+  setIsTTSActive: React.Dispatch<SetStateAction<boolean>>
+) => {
+  const [totalVoices, setTotalVoices] = useState<Speech.Voice[]>([])
+
+  useEffect(() => {
+    const getVoices = async () => {
+      const voices = await Speech.getAvailableVoicesAsync()
+      setTotalVoices(voices)
+    }
+
+    getVoices()
+  }, [])
+
+  const speak = useCallback(() => {
+    Speech.stop()
+    setIsTTSActive(true)
+    Speech.speak(text, {
+      voice: totalVoices[0]?.identifier,
+      onDone: () => {
+        setIsTTSActive(false)
+      }
+    })
+  }, [text, totalVoices])
+
+  return {
+    speak
+  }
 }
 
 export default useTTS
-
-const styles = StyleSheet.create({})
